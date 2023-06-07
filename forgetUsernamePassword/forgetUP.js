703485144d8090fd136188097a61b7962103c703
Samarth Giram <103664743+SamarthGiram@users.noreply.github.com>
2023-06-08 00:37:47 +0530

    Add files via upload

--- forgetUsernamePassword/forgetUP.js
+++ forgetUsernamePassword/forgetUP.js
@@ -1,34 +1,105 @@
-let mobile;
+var mobile;
+var OTP;
+var teleUser;
+var data;
+var count;
 
-async function TwilioNameLookup() {
-    var myHeaders = new Headers();
-myHeaders.append("apikey", "iTcC6eAK82uUzZRO8DDiKWzvl08bu8b1");
-console.log(localStorage.getItem('mobile'));
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
-const keys = Object.keys(localStorage);
-        keys.forEach((key) => {
-            if(document.getElementById('rollno').value == key){
-                const values = localStorage.getItem(key).split(",");
-                mobile = values[5].replace(/"mobile":|"/gi,'');
-            }})
-var no = mobile;
-if (document.getElementById('mobile').value == no) {
-    fetch("https://api.apilayer.com/number_verification/validate?number="+no, requestOptions)
-  .then(response => response.text())
-  .then(result =>{ console.log(result);})
-  .catch(error => console.log('error', error));
-    
-} else {
-    alert("Wrong mobile number.")
+window.onload = document.getElementById("verify").style.background = "gray";
+function onInput(){
+    teleUser = document.getElementById('username').value;
+    mobile = document.getElementById('mobile').value;
+    validation();
+}
+function validation(){
+  var color = document.getElementById("verify").style.color;
+    if(teleUser.length >= 4 && mobile.length == 10 ){
+      console.log("if");
+      document.getElementById("verify").disabled = false;
+    document.getElementById("verify").style.background = color;
+    }else{
+      document.getElementById("verify").style.background = "gray";
+      document.getElementById("verify").disabled = true;
+    }
 }
 
-  }
+
 
   function navigate(){
     window.location.replace('../index.html');
   }
 
+function otpWindow(){
+  let url = "https://api2.callmebot.com/txt/login.php";
+  window.open(url, "_blank");
+  openNewWindow();
+}
+
+function generateRandomText(length) {
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var text = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    text += characters.charAt(randomIndex);
+  }
+  return text;
+}
+
+function openNewWindow() {
+  var username = teleUser;
+  var customText = generateRandomText(4);
+  OTP = customText;
+  var url = "https://api.callmebot.com/text.php?user=@" + username + "&text=" + encodeURIComponent(customText);
+  fetch(url)
+    .then(function(response) {
+      // Handle the response
+      console.log("Request sent successfully.");
+    })
+    .catch(function(error) {
+      // Handle any errors
+      console.log("Error sending the request:", error);
+    });
+}
+
+function Verify(){
+  let str = '';
+  console.log(data);
+  for (let index = 0; index < count; index++) {
+   if(document.getElementById('mobile').value == data[index].mobile ){
+      str = `username: ${data[index].username}
+    password: ${data[index].password}`
+   }
+    
+  }
+  if(OTP == document.getElementById('otp').value){
+    var username = teleUser;
+  var customText = str;
+  var url = "https://api.callmebot.com/text.php?user=@" + username + "&text=" + encodeURIComponent(customText);
+  fetch(url)
+    .then(function(response) {
+      // Handle the response
+      console.log("Request sent successfully.");
+    })
+    .catch(function(error) {
+      // Handle any errors
+      console.log("Error sending the request:", error);
+    });
+  } 
+}
+
+const request = window.indexedDB.open('AdminData', 1);
+
+request.onerror = function (event) {
+  // Handle errors
+};
+
+request.onsuccess = function (event) {
+  const db = event.target.result;
+  const transaction = db.transaction(['Admins'], 'readonly');
+  const objectStore = transaction.objectStore('Admins');
+
+  const getAllRequest = objectStore.getAll();
+
+  getAllRequest.onsuccess = function (event) {
+    data = event.target.result;
+    count = data.length;
+  }}
